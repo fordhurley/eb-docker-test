8dc822402183af111703f7850d43504991e50533
Ford Hurley <ford.hurley@gmail.com>
2015-02-01 09:37:46 -0500

    Pull a var out of the environment

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var PORT = 8080;
 var app = express();
 app.get('/', function (req, res) {
   console.log('Request received');
-  res.send('Yo, what up?\n');
+  res.send('Hi, ' + process.env.NAME + '.\n');
 });
 
 app.listen(PORT);
